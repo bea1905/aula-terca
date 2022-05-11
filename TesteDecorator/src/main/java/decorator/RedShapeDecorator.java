f73747ccbd3727680b5f148b89508a5d9a5b2957
Lab.Academico <Lab.Academico@UNS12DT100020.unisul.br>
2022-05-10 21:32:10 -0300

    aa

--- TesteDecorator/src/main/java/decorator/RedShapeDecorator.java
+++ TesteDecorator/src/main/java/decorator/RedShapeDecorator.java
@@ -3,12 +3,12 @@ package decorator;
 import interfaces.Shape;
 
 public class RedShapeDecorator extends ShapeDecorator {
-
+//classe desenho vermelho 
 	public RedShapeDecorator(Shape decorarShape) {
 		super(decorarShape);
 	}
 	
-	
+
 	public void setRedBorder() {
 		System.out.println("Borda vermelha");
 	}
